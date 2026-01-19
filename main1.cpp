6c89a42f76828de6acce15e272d388d7430feeac
Husnain Ali <husnainalimughal588@gmail.com>
2026-01-19 16:22:10 +0500

    reverse each word

--- main1.cpp
+++ main1.cpp
@@ -1,12 +1,14 @@
 #include <iostream>
 using namespace std;
 
+//Reverse a string
+
 int main() {
 
     string a = "my name is husnain";
     int n = a.length();
 
-    for(int i=n; i>=0; i--){
+    for(int i=n-1; i>=0; i--){
         cout<<a[i];
     }
 
