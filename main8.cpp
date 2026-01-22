4886c08b43c61f08ff8a175ec5190ea0a51e0401
Husnain Ali <husnainalimughal588@gmail.com>
2026-01-22 17:32:32 +0500

    g++ main10.cpp -o main10 && ./main10

--- main8.cpp
+++ main8.cpp
@@ -26,3 +26,33 @@ int main () {
 
     return 0;
 }
+
+//anotherway
+
+// #include <iostream>
+// #include <unordered_set>
+// using namespace std;
+
+// int nonrepeatingSubstring (string s) {
+//     int left=0;
+//     int right=0;
+//     int count = 0;
+//     int maxLen = 0;
+//     unordered_set<char> st;
+//     while(left<s.length() && right<s.length()){
+//         while(st.find(s[right])!=st.end()){
+//             st.erase(s[left]);
+//             left++;
+//         }
+//         st.insert(s[right]);
+//         right++;
+//         count=right-left;
+//         maxLen = max(maxLen,count);
+//     }
+//     return maxLen;
+// }
+
+// int main() {
+//     cout<<nonrepeatingSubstring("pwwkew");
+//     return 0;
+// }
