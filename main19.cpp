bb118fc0829a8f78ebfcffff19541779129aaa8f
Husnain Ali <husnainalimughal588@gmail.com>
2026-01-27 19:23:23 +0500

    random

--- main19.cpp
+++ main19.cpp
@@ -1,27 +1,25 @@
 #include <iostream>
 using namespace std;
 
-//replace spaces with %20 like urlify
-
 void urlify(string &s) {
-    int originalLength = s.length();
-    int spaceCount = 0;
-
-    for(int i=0; i<originalLength; i++) {
-        if(s[i]==' ') {
-            spaceCount++;
+    int OriginalLength = s.length();
+    int countSpace = 0;
+    
+    //count spaces 
+    for(int i=0; i<OriginalLength-1; i++) {
+        if(s[i]==' '){
+            countSpace++;
         }
     }
+    if(countSpace==0) return;
 
-    if(spaceCount==0){
-        return;
-    }
+    //initialize new length
+    int newLength = OriginalLength + (countSpace*2);
+    int j=newLength-1;
 
-    int newLength=originalLength+(spaceCount*2);
-    int j = newLength-1;
     s.resize(newLength);
 
-    for(int i = originalLength-1; i>=0; i--){
+    for(int i=OriginalLength-1; i>=0; i--) {
         if(s[i]==' '){
             s[j]='0';
             s[j-1]='2';
@@ -32,12 +30,12 @@ void urlify(string &s) {
             j--;
         }
     }
+
 }
 
 int main() {
-    string str = "get pictures from google";
+    string str = "hello world gemini";
     urlify(str);
-    cout<<str;
-    
+    cout << "URLified: '" << str << "'" << endl;
     return 0;
 }
